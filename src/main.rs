cd0ca39ee928a381d5d321ae83287de213ddefb1
Ronnel Palencia <ronnel.m.palencia@gmail.com>
2025-03-31 22:24:28 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String, 
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path)
+    .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+}
